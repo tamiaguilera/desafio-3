fbc49a96623c97b0a7d91c339af001fb75baa5bc
tamiaguilera <tami.aguilera@gmail.com>
2022-08-03 18:18:14 -0400

    parte2 lista

--- parte2/assets/js/scripts.js
+++ parte2/assets/js/scripts.js
@@ -1,8 +1,17 @@
-const img = document.getElementById("img");
-    img.addEventListener("click", () => {
-        if(img.classList.contains("border")){
-            img.classList.remove("border")
-        }else {
-            img.classList.add("border")
-        }
- });
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+    const sticker1 = document.getElementById("sticker-1").value;
+    const sticker2 = document.getElementById("sticker-2").value;
+    const sticker3 = document.getElementById("sticker-3").value;
+    const msg = document.getElementById("msg");
+
+    const total = Number(sticker1) + Number(sticker2) + Number(sticker3);
+    
+    if (total <= 10) {
+        msg.innerHTML = `Llevas ${total} stickers`;
+      } else {
+        msg.innerHTML = "Llevas muchos stickers";
+      }
+    });
+    
+ 
--- parte3/assets/js/scripts.js
+++
@@ -1,8 +0,0 @@
-const img = document.getElementById("img");
-    img.addEventListener("click", () => {
-        if(img.classList.contains("border")){
-            img.classList.remove("border")
-        }else {
-            img.classList.add("border")
-        }
- });
